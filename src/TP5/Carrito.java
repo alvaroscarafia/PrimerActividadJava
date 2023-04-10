c81eba5ead6cf62458315053fadac2025f855413
Alvaro Scarafia <alvaro_s.d@hotmail.com>
2023-04-10 10:59:41 -0300

    TP5 -Carrito- Termiado

--- src/TP5/Carrito.java
+++ src/TP5/Carrito.java
@@ -1,41 +1,73 @@
 package TP5;
+import java.util.Scanner;
+
+class Producto {
+    String nombreP;
+    int cantidad;
+    double precioUnitario;
+
+    public Producto(String nombreP, int cantidad, double precioUnitario){
+        this.nombreP = nombreP;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+}
+
+class itemCarrito extends Producto{
+    double precioTotalP;
+
+    public itemCarrito(String nombreP, int cantidad, double precioUnitario, double precioTotalP){
+
+        super(nombreP, cantidad, precioUnitario);
+        this.precioTotalP = precioTotalP;
+    }
+
+    public double metodoSumaTotal(){
+        precioTotalP = precioUnitario * cantidad;
+        return precioTotalP;
+    }
+}
+
+class Descuento extends itemCarrito {
+    double desc;
+
+    public Descuento(String nombreP, int cantidad, double precioUnitario, double precioTotalP, double desc){
+        super(nombreP, cantidad, precioUnitario, precioTotalP);
+        this.desc = desc;
+    }
+
+}
 
 public class Carrito {
-    public String nombre;
-    public int precio;
-    public int cantidad;
-    public static final Double descuento = 0.8;
-
-    public Carrito(String nombre, int precio, int cantidad) { // CONSTRUCTOR
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-
-}
-
-class Cliente extends Carrito{
-    String cliente;
-        public Cliente (String nombre,  int precio, int cantidad, String cliente){
-            super(nombre,precio,cantidad);
-            this.cliente = cliente;
-        }
-}
-
-
-class Main {
     public static void main(String[] args) {
-        Carrito producto1 = new Carrito("Banana", 200, 1);
-        Carrito producto2 = new Carrito("Lechuga", 100, 1);
-        Carrito producto3 = new Carrito("Coca Cola", 500, 1);
-        Cliente cliente1 = new Cliente("Manzana", 300, 3,"Alvaro");
 
-        System.out.println(producto1.nombre);
-        System.out.println(producto3.nombre);
-        System.out.println(producto2.nombre);
-        System.out.println(cliente1.cliente);
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese producto, cantidad y precio por unidad");
+        Producto compra = new Producto(
+                teclado.next(),
+                teclado.nextInt(),
+                teclado.nextInt()
+        );
+        itemCarrito itemc1 = new itemCarrito(
+                compra.nombreP,
+                compra.cantidad,
+                compra.precioUnitario,
+                compra.cantidad*compra.precioUnitario
+        );
+        System.out.println("Precio sin descuento: " + itemc1.precioTotalP);
 
+        if(itemc1.precioTotalP>1000){
+            Descuento compraDesc = new Descuento(
+                    itemc1.nombreP,
+                    itemc1.cantidad,
+                    itemc1.precioUnitario,
+                    itemc1.precioTotalP,
+                    itemc1.precioTotalP - (itemc1.precioTotalP * 0.10)
+            );
+            double total = compraDesc.desc;
+            System.out.println("Total: "+ total);
+        }else{
+            System.out.println("No hay descuento porque no supera los $1000");
+        }
     }
-
-
 }
